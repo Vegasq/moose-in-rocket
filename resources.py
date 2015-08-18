bacbe4d35fe4416810419ce5cac62b6be538fca8
Mykola Yakovliev <vegasq@gmail.com>
2015-08-17 22:34:53 -0500

    Resources moved tp separate folder

--- resources.py
+++ resources.py
@@ -1,52 +1,52 @@
 from kivy.metrics import dp, sp
 
 rocket = {
-    'sprite1': "rocket_1.png",
-    'sprite2': "rocket_2.png",
+    'sprite1': "resources/rocket_1.png",
+    'sprite2': "resources/rocket_2.png",
     'width': dp(80),
     'height': dp(135)
 }
 
 background3 = {
-    'sprite': "background_3.png",
+    'sprite': "resources/background_3.png",
     'width': dp(1024),
     'height': dp(768)
 }
 background2 = {
-    'sprite': "background_2.png",
+    'sprite': "resources/background_2.png",
     'width': dp(1085),
     'height': dp(1153)
 }
 background1 = {
-    'sprite': "background_1.png",
+    'sprite': "resources/background_1.png",
     'width': dp(1018),
     'height': dp(1493)
 }
 
 basement = {
-    'sprite': "basement.png",
+    'sprite': "resources/basement.png",
     'width': dp(1024),
     'height': dp(3072)
 }
 
 asteroid = {
-    'sprite': 'asteroid_1.png',
+    'sprite': 'resources/asteroid_1.png',
     'width': dp(551),
     'height': dp(166)
 }
 
 asteroid_round = {
-    'sprite': 'asteroid_2.png',
+    'sprite': 'resources/asteroid_2.png',
     'width': dp(150),
     'height': dp(155)
 }
 
 ruby = {
-    'sprite': 'crystal_1.png'
+    'sprite': 'resources/crystal_1.png'
 }
 
 score = {
-    'sprite': 'counter_1.png',
+    'sprite': 'resources/counter_1.png',
     'width': dp(100),
     'height': dp(70)
 }
